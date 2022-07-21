31319738335537cbfd975e4c47b5f0856062aca4
prastoin <tkesray@gmail.com>
2022-07-21 15:11:56 +0200

    fix: typescript build

--- jest.setup.tsx
+++ jest.setup.tsx
@@ -12,4 +12,7 @@
 
 // // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 // jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
 jest.useFakeTimers();
+
+export {};
