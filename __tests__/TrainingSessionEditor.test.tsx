881eaa7951676ddb24d9ed363212e7b312bed9c0
prastoin <tkesray@gmail.com>
2022-08-13 17:23:15 +0200

    test: improve retrieve program on start test

--- __tests__/TrainingSessionEditor.test.tsx
+++ __tests__/TrainingSessionEditor.test.tsx
@@ -9,43 +9,10 @@ import {
   renderApp,
   SERVER_ENDPOINT,
   waitFor,
-  within,
 } from "../tests/test.utils";
 import { RetrieveUserBodyBuildingProgramResponseBody } from "../types";
 
 test("On app start user program is retrieved", async () => {
-  const bodyBuildingProgram = getBodyBuildingProgram();
-  server.use(
-    rest.post<
-      undefined,
-      Record<string, never>,
-      RetrieveUserBodyBuildingProgramResponseBody
-    >(`${SERVER_ENDPOINT}/retrieve-program`, (_req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(bodyBuildingProgram));
-    })
-  );
-
-  const screen = renderApp();
-
-  await screen.findByTestId(/home-screen-container/i);
-
-  const programBuilderBottomTab = await screen.findByTestId(
-    `program-builder-bottom-tab`
-  );
-
-  fireEvent.press(programBuilderBottomTab);
-
-  await screen.findByTestId("program-builder-screen-container");
-
-  await waitFor(() => {
-    const allTrainingSessionContainer = getAllTrainingSessionContainer(screen);
-    expect(allTrainingSessionContainer.length).toBe(
-      bodyBuildingProgram.trainingSessions.length
-    );
-  });
-});
-
-test.only("User can re-enter training session editor name", async () => {
   const bodyBuildingProgram = getBodyBuildingProgram();
   server.use(
     rest.post<
@@ -67,17 +34,56 @@ test.only("User can re-enter training session editor name", async () => {
 
   await screen.findByTestId("program-builder-screen-container-visible");
 
-  const firstTrainingSession = bodyBuildingProgram.trainingSessions[0];
-  const firstTrainingSessionContainer = await waitFor(
-    async () =>
-      await screen.findByTestId(
-        `training-session-container-${firstTrainingSession.uuid}`
-      )
+  const allTrainingSessionContainer = await waitFor(() => {
+    const allTrainingSessionContainer = getAllTrainingSessionContainer(screen);
+    expect(allTrainingSessionContainer.length).toBe(
+      bodyBuildingProgram.trainingSessions.length
+    );
+
+    return allTrainingSessionContainer;
+  });
+
+  await Promise.all(
+    bodyBuildingProgram.trainingSessions.map(async (trainingSession) => {
+      const oneIsMatching = allTrainingSessionContainer.find(
+        (container) =>
+          container.props.testID ===
+          `training-session-container-${trainingSession.uuid}`
+      );
+      expect(oneIsMatching).toBeTruthy();
+    })
+  );
+});
+
+test("User can re-enter training session editor name", async () => {
+  const bodyBuildingProgram = getBodyBuildingProgram();
+  server.use(
+    rest.post<
+      undefined,
+      Record<string, never>,
+      RetrieveUserBodyBuildingProgramResponseBody
+    >(`${SERVER_ENDPOINT}/retrieve-program`, (_req, res, ctx) => {
+      console.log("MOCK IS HIT");
+      return res(ctx.status(200), ctx.json(bodyBuildingProgram));
+    })
   );
 
-  const firstTrainingSessionName = await within(
-    firstTrainingSessionContainer
-  ).findByText(firstTrainingSession.trainingSessionName);
+  const screen = renderApp();
+
+  await screen.findByTestId("home-screen-container-visible");
+
+  const programBuilderBottomTab = await getProgramBuilderTabIcon(screen);
+
+  fireEvent.press(programBuilderBottomTab);
+
+  await screen.findByTestId("program-builder-screen-container-visible");
+
+  const firstTrainingSession = bodyBuildingProgram.trainingSessions[0];
+  const allTrainingSessionContainer = getAllTrainingSessionContainer(screen);
+
+  const firstTrainingSessionName = await screen.findByText(
+    firstTrainingSession.trainingSessionName
+  );
   fireEvent.press(firstTrainingSessionName);
 
   await screen.findByTestId(
@@ -96,9 +102,7 @@ test.only("User can re-enter training session editor name", async () => {
 
   // Hitting training session name again
   fireEvent.press(
-    await within(firstTrainingSessionContainer).findByText(
-      firstTrainingSession.trainingSessionName
-    )
+    await screen.findByText(firstTrainingSession.trainingSessionName)
   );
 
   await screen.findByTestId(
--- services/ProgramBuilderService.ts
+++ services/ProgramBuilderService.ts
@@ -3,9 +3,7 @@ import { SERVER_ENDPOINT } from "../tests/test.utils";
 import { RetrieveUserBodyBuildingProgramResponseBody } from "../types";
 
 export async function sendRetrieveUserBodyBuildingProgram(): Promise<RetrieveUserBodyBuildingProgramResponseBody> {
-  const response = await redaxios.post(
-    `${SERVER_ENDPOINT}/authentication/sign-in`
-  );
+  const response = await redaxios.post(`${SERVER_ENDPOINT}/retrieve-program`);
   const responseBody = response.data;
   const parsedResponseBody =
     RetrieveUserBodyBuildingProgramResponseBody.parse(responseBody);
