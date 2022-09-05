eb40aa83654b3532697fb50a5f26dad5e02e1e3c
prastoin <tkesray@gmail.com>
2022-09-05 10:57:24 +0200

    feat: create stack navigator and first screens for session tracker

---
+++ screens/SessionTracker/SessionTrackerIndex.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { Button, Text } from "react-native";
+import { useTailwind } from "tailwind-rn";
+import AppScreen from "../../components/AppScreen";
+import { SessionTrackIndexScreenProps } from "../../navigation/RootStack";
+
+export const SessionTrackerIndex: React.FC<SessionTrackIndexScreenProps> = ({
+  navigation,
+}) => {
+  const tailwind = useTailwind();
+
+  return (
+    <AppScreen testID="session-tracker-index-container">
+      <Text style={tailwind("text-blue-600")}>
+        Session Tracker screen index
+      </Text>
+      <Button
+        onPress={() => {
+          navigation.navigate("SessionTrackerCreationForm");
+        }}
+        title="Start next training session tracker"
+      ></Button>
+    </AppScreen>
+  );
+};
