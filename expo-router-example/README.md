14784d66e02d73e2d1051676b68e296d402926ae
prastoin <tkesray@gmail.com>
2023-07-20 16:54:14 +0200

    feat: tailwind css with expo-router fix

---
+++ expo-router-example/README.md
@@ -0,0 +1,3 @@
+# Resources
+- frozen tailwindcss version please see https://github.com/marklawlor/nativewind/issues/501
+- followed nativewind docs https://www.nativewind.dev/quick-starts/expo
--- expo-router-example/babel.config.js
+++ expo-router-example/babel.config.js
@@ -4,7 +4,8 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+     "nativewind/babel", 
+     'expo-router/babel',
     ],
   };
 };
