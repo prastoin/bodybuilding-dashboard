b1a1a479fa6ae994784b342feff968f979397624
prastoin <tkesray@gmail.com>
2023-07-20 17:41:04 +0200

    feat: first global context

--- expo-router-example/app/(tabs)/index.tsx
+++ expo-router-example/app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { useAppContext } from "@/context/appContext";
 import { Text, View } from "react-native";
 
 export default function TabOneScreen() {
+  const appContext = useAppContext();
+
   return (
     <View className='w-80 bg-white shadow rounded'>
-      <Text className="text-blue-600 font-bold">Tab One</Text>
+      <Text className="text-blue-600 font-bold">Tab One {appContext.test}</Text>
     </View>
   );
 }
