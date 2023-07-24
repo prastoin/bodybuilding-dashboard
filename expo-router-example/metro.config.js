f8da689da55f22193e068f99fe083d13c7c6767c
prastoin <tkesray@gmail.com>
2023-07-24 22:53:51 +0200

    fix: metro unable to resolve react-hook-form

--- expo-router-example/metro.config.js
+++ expo-router-example/metro.config.js
@@ -2,9 +2,18 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
+const defaultConfig = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 });
+
+// Added mjs and cjs source for react-hook-form
+// Please refer to https://github.com/infinitered/ignite/issues/1995
+const config = {
+  ...defaultConfig,
+  resolver: {
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'mjs', 'cjs'],
+  }
+}
 
 module.exports = config;
