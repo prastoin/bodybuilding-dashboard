0b5c90da321e2ec6b5e61eefd7a1a24637f97872
prastoin <tkesray@gmail.com>
2023-07-25 23:09:49 +0200

    feat: migrate hooks

---
+++ expo-router-example/hooks/useExerciseCreationFormActor.ts
@@ -0,0 +1,24 @@
+import { useSelector } from "@xstate/react";
+import invariant from "invariant";
+import { ExerciseCreationFormActorRef } from "../machines/ExerciseCreationFormMachine";
+import { useTrainingSessionActorRef } from "./useTrainingSessionActorRef";
+
+export function useExerciseCreationFormActor(
+  trainingSessionId: string
+): ExerciseCreationFormActorRef | undefined {
+  const trainingSessionActor = useTrainingSessionActorRef(trainingSessionId);
+
+  invariant(
+    trainingSessionActor !== undefined,
+    "training Session actor is undefined"
+  );
+
+  const exerciseCreationForm = useSelector(
+    trainingSessionActor,
+    (state) => state.children.ExerciseCreationForm as
+    | ExerciseCreationFormActorRef
+    | undefined
+  );
+
+  return exerciseCreationForm;
+}
