6fd2dd9e8d137ac064cb491327dc389294d9c7f1
prastoin <tkesray@gmail.com>
2023-07-27 00:12:23 +0200

    chore: install and mock react-native-reanimated

--- expo-router-example/jest.setup.ts
+++ expo-router-example/jest.setup.ts
@@ -1,14 +1,14 @@
 // import 'react-native-gesture-handler/jestSetup';
 
-// jest.mock('react-native-reanimated', () => {
-//   const Reanimated = require('react-native-reanimated/mock');
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
 
-//   // The mock for `call` immediately calls the callback which is incorrect
-//   // So we override it with a no-op
-//   Reanimated.default.call = () => {};
+  // The mock for `call` immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => { };
 
-//   return Reanimated;
-// });
+  return Reanimated;
+});
 
 // Polyfill "window.fetch" used in the React component.
 import "whatwg-fetch";
