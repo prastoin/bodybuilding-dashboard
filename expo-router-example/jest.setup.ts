9e269d9aaf48611caa46d3bc717654f4e8a3671a
prastoin <tkesray@gmail.com>
2023-07-26 23:04:13 +0200

    test: jest-setup

--- expo-router-example/jest.setup.ts
+++ expo-router-example/jest.setup.ts
@@ -35,5 +35,5 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-jest.spyOn(console, "warn").mockImplementation();
-jest.spyOn(console, "error").mockImplementation();
+// jest.spyOn(console, "warn").mockImplementation();
+// jest.spyOn(console, "error").mockImplementation();
