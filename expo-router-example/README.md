4aa98fb15ab57d295b534cb7e7b4f1d307298bff
prastoin <tkesray@gmail.com>
2023-08-08 15:47:40 +0200

    chore: remove expo-router-example folder

--- expo-router-example/README.md
+++
@@ -1,3 +0,0 @@
-# Resources
-- frozen tailwindcss version please see https://github.com/marklawlor/nativewind/issues/501
-- followed nativewind docs https://www.nativewind.dev/quick-starts/expo
--- expo-router-example/__tests__/App.test.tsx
+++
@@ -1,6 +0,0 @@
-import { renderApp } from "../tests/test.utils";
-
-test("Home page is the app landing page", async () => {
-  const screen = renderApp();
-  await screen.findByTestId("home-screen-container");
-});
