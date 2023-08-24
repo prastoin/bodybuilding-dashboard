1aaa200b8ffb3f0b58b6918517540da999352b28
prastoin <tkesray@gmail.com>
2023-08-24 16:36:55 +0200

    feat: suggest upcoming session to track and handle picking a session

--- app/pickerModal.tsx
+++ app/pickerModal.tsx
@@ -1,9 +1,19 @@
 import AppScreen from "@/components/AppScreen";
 import { useAppContext } from "@/context/appContext";
 import { useBeforeRemove } from "@/hooks/useBeforeRemove";
+import { SessionMachineState, SessionMachineContext } from "@/machines/SessionMachine";
+import { fromSessionMachineContextToSession } from "@/machines/Tracker/utils";
+import { Session } from "@/types";
 import { useActor } from "@xstate/react";
 import { StatusBar } from "expo-status-bar";
-import { Platform, Text } from "react-native";
+import { FlatList, Platform, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+interface SessionCardCtaProps {
+    session: SessionMachineContext,
+    onSessionCardPress: (session: SessionMachineContext) => void
+}
+const SessionCardCta: React.FC<SessionCardCtaProps> = ({ session, onSessionCardPress }) => <TouchableOpacity onPress={() => onSessionCardPress(session)}><Text>{session.name}</Text></TouchableOpacity>
 
 export default function PickScreen() {
     const { trackerService, programService } = useAppContext();
@@ -12,13 +22,40 @@ export default function PickScreen() {
     const { context: { sessionTrackerList } } = trackerState
     const { context: { sessionActorRefList } } = programState
 
+    const onSessionCardPress = (sessionMachineContext: SessionMachineContext) => {
+        const session = fromSessionMachineContextToSession(sessionMachineContext)
+        sendToTrackerMachine({
+            type: "USER_PICKED_SESSION",
+            session
+        })
+    }
+
     useBeforeRemove(() => sendToTrackerMachine("USER_CANCELLED_TRACKING_SESSION_PICKER"))
+    const sessionSnapshotList = sessionActorRefList.map((session) => session.getSnapshot()).filter((el: SessionMachineState | undefined): el is SessionMachineState => el !== undefined).map((el) => el.context)
+    const latestTrackedSessionId = sessionTrackerList.length > 0 ? sessionTrackerList.slice(-1)[0].sessionId : undefined
+
+    const latestTrackedIndex = sessionSnapshotList.findIndex(({ uuid }) => uuid === latestTrackedSessionId);
+    const notFound = latestTrackedIndex === -1
+    const defaultIndex = 0;
+    const nextTrackedIndex = notFound ? defaultIndex : (latestTrackedIndex + 1) % (sessionSnapshotList.length)
+    const nextTrackedSession = sessionSnapshotList.length >= nextTrackedIndex ? sessionSnapshotList[nextTrackedIndex] : undefined;
 
     return (<AppScreen testID="picker-modal-screen-container">
-        <Text>
+        <Text className="font-bold">
             This is the picker modal !
         </Text>
+        {nextTrackedSession !== undefined && <View>
+            <Text>
+                Upcoming session ?:
+            </Text>
+            <SessionCardCta session={nextTrackedSession} onSessionCardPress={onSessionCardPress} />
+        </View>}
         {/* Use a light status bar on iOS to account for the black space above the modal */}
+        <Text className="font-bold">All available sessions:</Text>
+        <FlatList<SessionMachineContext>
+            data={sessionSnapshotList}
+            renderItem={({ item: session }) => <SessionCardCta session={session} onSessionCardPress={onSessionCardPress} />}
+            keyExtractor={({ uuid }) => uuid} />
         <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </AppScreen>);
+    </AppScreen >);
 }
--- machines/Tracker/utils.ts
+++ machines/Tracker/utils.ts
@@ -1,4 +1,6 @@
-import { Exercise, ExerciseTracker } from "@/types";
+import { Exercise, ExerciseTracker, Session } from "@/types";
+import { ExerciseMachineState } from "../ExerciseMachine";
+import { SessionMachineContext } from "../SessionMachine";
 
 export function fromExerciseListToExerciseTracker(exerciseList: Exercise[]): ExerciseTracker[] {
     const transpiler = ({ load, name, repCounter, rest, setCounter, uuid: exerciseId }: Exercise) => {
@@ -16,4 +18,13 @@ export function fromExerciseListToExerciseTracker(exerciseList: Exercise[]): Exe
     }
 
     return exerciseList.map(transpiler);
+}
+
+export function fromSessionMachineContextToSession({ uuid, name, exerciseActorRefList }: SessionMachineContext): Session {
+    const exerciseList = exerciseActorRefList.map((actor) => actor.getSnapshot()).filter((el: undefined | ExerciseMachineState): el is ExerciseMachineState => el !== undefined).map(el => el.context)
+    return {
+        exerciseList,
+        name,
+        uuid
+    }
 }
