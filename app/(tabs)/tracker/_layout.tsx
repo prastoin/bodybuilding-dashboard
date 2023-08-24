a94c6a7f95bf7db68074e168625a21140204c55d
prastoin <tkesray@gmail.com>
2023-08-24 11:24:35 +0200

    feat: first tracker stack screen

--- app/(tabs)/tracker/_layout.tsx
+++ app/(tabs)/tracker/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 
-export default function SessionTrackerLayout() {
+export default function TrackerLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
