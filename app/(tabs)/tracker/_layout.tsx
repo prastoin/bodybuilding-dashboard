0e939038fa1b6cc557f5e89727dd872190325650
prastoin <tkesray@gmail.com>
2023-08-25 13:00:53 +0200

    feat: create screen for tracking session review

--- app/(tabs)/tracker/_layout.tsx
+++ app/(tabs)/tracker/_layout.tsx
@@ -4,6 +4,7 @@ export default function TrackerLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="[sessionTrackerId]" options={{ headerShown: false }} />
     </Stack>
   );
 }
